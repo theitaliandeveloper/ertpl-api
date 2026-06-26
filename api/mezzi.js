299b636e89400798e3d32a20e893948b4649fad8
theitaliandeveloper <theitaliandeveloper@proton.me>
2026-06-26 18:11:50 +0200

    Lista mezzi

---
+++ api/mezzi.js
@@ -0,0 +1,81 @@
+// Roba da non modificare assolutamente
+export const config = {
+  runtime: "edge"
+};
+
+const corsHeaders = {
+  //"Access-Control-Allow-Origin": "https://ertpl.pages.dev",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type"
+};
+
+// Lista dei server modificabile
+const servers = [
+    {
+      name: "Serverissimo",
+      url: "https://dbiface.serverissimo.com/health"
+    },
+    {
+      name: "DaniLab",
+      url: "https://ertpl-db.daninet.freeddns.org/health"
+    },
+    {
+      name: "Vichingo455",
+      url: "https://api.vichingo455.qzz.io/ertpl/health"
+    }
+  ];
+
+// Altra roba da modificare solamente in caso di bisogno
+async function checkServer(server, timeoutMs = 5000) {
+  try {
+    const controller = new AbortController();
+    const timeout = setTimeout(() => controller.abort(), timeoutMs);
+    const response = await fetch(server.url, {
+      headers: {
+        Range: "bytes=0-0"
+      },
+      signal: controller.signal
+    });
+    clearTimeout(timeout);
+    if (!response.ok) return false;
+    const data = await response.json();
+    return data.status == "ok";
+  } catch {
+    return false;
+  }
+}
+
+export default async function handler() {
+  for (const server of servers) {
+    if (await checkServer(server)) {
+      return new Response(
+        JSON.stringify({
+          status: "ok",
+          server: server.name,
+          url: server.url.replace("/health","")
+        }),
+        {
+          status: 200,
+          headers: {
+            ...corsHeaders,
+            "Content-Type": "application/json",
+            "Cache-Control": "public, s-maxage=30"
+          }
+        }
+      );
+    }
+  }
+
+  return new Response(
+    JSON.stringify({ status: "offline" }),
+    {
+      status: 503,
+      headers: {
+        ...corsHeaders,
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store"
+      }
+    }
+  );
+}

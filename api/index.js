4e4fccc43a3b65737488a29e6b9e121d9ba6e2c3
theitaliandeveloper <theitaliandeveloper@proton.me>
2026-07-30 14:51:58 +0200

    AGPL license

--- api/index.js
+++ api/index.js
@@ -1,3 +1,21 @@
+/*
+ER-TPL High Availability API
+Copyright (C) 2025-26 Vichingo455
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // Roba da modificare solo in caso di bisogno
 
 export const config = {
